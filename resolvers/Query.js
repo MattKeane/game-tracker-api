da07d571a405a58554490c878d9a5e47eb31798c
Matt Keane <mattpkeane@gmail.com>
2021-01-20 14:25:45 -0600

    added current user query

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,7 +1,17 @@
+// import models
+const User = require("../models/user")
+
 function test() {
 	return "Server is working!"
 }
 
+async function currentUser(parent, args, context) {
+	const { userId } = context
+	const user = await User.findById(userId)
+	return user
+}
+
 module.exports = {
 	test,
+	currentUser
 }
